0918b454477bb9c1450b4ce92ccc9a4b3d1f0de1
Sam Woolerton <samuelwoolerton@gmail.com>
2022-02-12 14:13:51 +1300

    Basic passages list

--- src/pages/ListPassages.tsx
+++ src/pages/ListPassages.tsx
@@ -1,10 +1,29 @@
+import { useState } from "@hookstate/core"
 import { Link } from "react-router-dom"
+import globalState from "state"
 
 export default function ListPassages() {
+  const global = useState(globalState)
+
   return (
     <div>
       <Link to="/passages/add">Add passage</Link>
-      List passages.
+
+      <div>
+        {global.passages.get().map(p => (
+          <Link to={`/passages/${p.id}`} className="flex">
+            <div>{p.book}</div>
+            <div>{p.chapter}</div>
+            <div>
+              {p.startVerse === p.endVerse
+                ? p.startVerse
+                : `${p.startVerse}:${p.endVerse}`}
+
+              <Link to={`/passages/${p.id}/attempt`}>Attempt</Link>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
